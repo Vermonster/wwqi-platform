ee1a650159fb5198d90aae860c6091b1b7750c6b
Meg Broughton <meg@vermonster.com>
2013-03-14 17:08:16 -0400

    configured rails' default generators

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,11 @@ module WwqiPlatform
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.generators do |g|
+      g.orm :active_record
+      g.template_engine :haml
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+    end
   end
 end
