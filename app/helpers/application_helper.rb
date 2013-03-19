77d9bc3140cc698798d717fc60369849a9db83ff
Gregory Salmon <salmonthefish@gmail.com>
2013-03-19 17:03:01 -0400

    add helper for flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 module ApplicationHelper
   def post_type_radio_options
     [['question', 'Question'], ['discussion', 'Discussion']]
@@ -14,5 +16,26 @@ module ApplicationHelper
     when Discussion
       "Submit Reply"
     end
+  end
+
+  def flash_messages
+    return if flash.empty?
+
+    css = {
+      alert: "alert-error",
+      success: "alert-success",
+      notice: "",
+    }
+    
+    content_tag(:div, id: 'flash-messages') do
+      flash.collect do |type, message|
+        content_tag(:div, class: "flash-messages") do
+          content_tag(:div, class: "alert #{css[type.intern]}") do
+            concat button_tag("×", :type => "button", :name => nil, :class => "close", :"data-dismiss" => "alert")
+            concat content_tag(:strong, message)
+          end
+        end
+      end.join('\n').html_safe
+    end    
   end
 end
