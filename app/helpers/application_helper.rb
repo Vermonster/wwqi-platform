2181259e1a4433034141ca3fc09975d01df54613
Gregory Salmon <salmonthefish@gmail.com>
2013-03-19 11:34:51 -0400

    create post form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def post_type_radio_options
+    [['question', 'Question'], ['discussion', 'Discussion']]
+  end
+
+  def post_private_radio_options
+    [['true', 'People I Choose'], ['false', 'Everyone']] 
+  end
 end
