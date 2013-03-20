a1ce03d15ee3eee01d89b0b83ab3d175d194f1d3
Gregory Salmon <salmonthefish@gmail.com>
2013-03-20 09:59:47 -0400

    add sample discussion to demo data

--- lib/demo_data.rb
+++ lib/demo_data.rb
@@ -34,21 +34,33 @@ module DemoData
   end
 
   def self.load_posts
-    user1 = User.where(first_name: 'Andrew').first
-    user2 = User.where(first_name: 'Steve').first
+    andrew = User.where(first_name: 'Andrew').first
+    steve = User.where(first_name: 'Steve').first
     
 
-    post = FactoryGirl.create(:question,
+    post1 = FactoryGirl.create(:question,
                        title: "What was the significance of the Qamar Taj Dawlatabadi's travels?",
                        details: "I am writing a paper on the Dawlatabadi family history, and have
                        come across numerous interesting documents about them on the WWQI site.
                        Interestingly, Qamar Taj Dawlatabadi seems to have traveled a lot. I wanted
                        to know if anyone knows the purpose of her travels?",
                        tag_list: 'Moluk-al-saltaneh, Isfahan',
-                       creator: user1)
+                       creator: andrew)
     FactoryGirl.create(:comment,
                        details: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Vivamus vitae risus vitae lorem iaculis placerat. Aliquam sit amet felis. Etiam congue. Donec risus risus, pretium ac, tincidunt eu, tempor eu, quam. Morbi blandit mollis magna. Suspendisse eu tortor. Donec vitae felis nec ligula blandit rhoncus. Ut a pede ac neque mattis facilisis. Nulla nunc ipsum, sodales vitae, hendrerit non, imperdiet ac, ante. Morbi sit amet mi. Ut magna. Curabitur id est. Nulla velit. Sed consectetuer sodales justo. Aliquam dictum gravida libero. Sed eu turpis. Nunc id lorem. Aenean consequat tempor mi. Phasellus in neque. Nunc fermentum convallis ligula.",
-                       commentable: post,
-                       user: user2)
+                       commentable: post1,
+                       user: steve)
+  
+    post2 = FactoryGirl.create(:discussion,
+                               title: "Guess what I had for breakfast today.",
+                               details: "Ok, guys. I woke up with such a headache. I think I'm 
+                               suffering from amnesia because I can't even remember what I had for
+                               breakfast today. What's your best guess?",
+                               tag_list: 'breakfast, amnesia',
+                               creator: steve)
+    FactoryGirl.create(:comment,
+                       details: "I'm guessing you had green eggs and ham. Am I close?",
+                       commentable: post2,
+                       user: andrew)
   end
 end
