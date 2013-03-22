c488bd885a47c0e7a01b828cffe58d53db9338b4
Gregory Salmon <salmonthefish@gmail.com>
2013-03-22 09:45:26 -0400

    add poltergeist as javascript driver

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'capybara/poltergeist'
+
+Capybara.javascript_driver = :poltergeist
 
 include Warden::Test::Helpers
 def login(user)
