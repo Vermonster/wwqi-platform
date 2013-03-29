8b3b7abda881b76804cd626d69d7505a7b7a81b4
Kyung Bae Park <kyungbae@gmail.com>
2013-03-29 14:39:12 -0400

    Add buttons using Bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,22 @@
 //= require bootstrap-tags
 //= require cocoon
 //= require_tree .
+
+// Javascript for Bootstraps radio buttons within Simple Form
+$(function() {
+  $('div.btn-group[data-toggle-name]').each(function() {
+    var group = $(this);
+    var form = group.parents('form').eq(0);
+    var name = group.attr('data-toggle-name');
+    var hidden = $('input[name="' + name + '"]', form);
+    $('button', group).each(function() {
+      var button = $(this);
+      button.on('click', function() {
+        hidden.val($(this).val());
+      });
+      if (button.val() == hidden.val()) {
+        button.addClass('active');
+      }
+    });
+  });
+});
