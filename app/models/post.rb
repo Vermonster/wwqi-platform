d71bd5da5952ce5da2ee8bbe0d3e454218de81e6
Kyung Bae Park <kyungbae@gmail.com>
2013-04-07 17:10:53 -0400

    Reverse Autocomplete implementation to use a hidden field

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,6 @@ class Post < ActiveRecord::Base
   has_many :followers, through: :followings, class_name: :User
   has_many :items, dependent: :destroy
   has_many :collaborators, dependent: :destroy
-  
   attr_accessible :title, :details, :item_related, :private, :creator_id, :type, :tag_list, :uploads_attributes, :items_attributes, :collaborators_attributes
 
   validates :title, :details, :creator_id, presence: true
