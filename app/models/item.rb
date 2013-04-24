1e177ed1af4a614c60a87ef489a3c5ad6eff7196
Gregory Salmon <salmonthefish@gmail.com>
2013-04-24 16:20:38 -0400

    make search attribute accessible

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :post
-  attr_accessible :url, :name, :thumbnail, :item_id
+  attr_accessible :url, :name, :thumbnail, :item_id, :search
   attr_accessor :search
 
   validates :url, presence: true
