4486c5c5fb8375b87a93fc9586f4da2f0de4f18a
Gregory Salmon <salmonthefish@gmail.com>
2013-04-26 13:52:05 -0400

    show error for duplicate item entries

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,6 +13,7 @@ class Post < ActiveRecord::Base
 
   validates :title, :details, :creator_id, presence: true
   validate :collaborator_duplication
+  validate :item_duplication
 
   accepts_nested_attributes_for :uploads, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :items, :reject_if => :not_item_related?, :allow_destroy => true
@@ -47,7 +48,15 @@ class Post < ActiveRecord::Base
     # If they are same, each collaborators are unique. If not, one or more
     # collaborators are added more than once. 
     if collaborators.length != num_of_unique 
-      errors.add(:collaborator, 'One or more recipients are added more than once.')
+      errors.add(:collaborators, 'You have duplicate collaborators listed.')
+    end
+  end
+
+  def item_duplication
+    unique = items.map(&:accession_no).uniq.length
+
+    if items.length != unique
+      errors.add(:items, 'You have duplicate items listed.')
     end
   end
 end
