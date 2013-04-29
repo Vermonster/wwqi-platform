a323c74ad35c04c26071d6deaf02ee245fac8e9f
Eduardo Gutierrez <eduardo@vermonster.com>
2013-04-29 14:56:18 -0400

    Remove duplicate methods in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,30 +32,4 @@ class User < ActiveRecord::Base
   def fullname
     "#{first_name} #{last_name}"
   end
-
-  def followed_questions_and_discussions
-    followed_posts.where("type = 'Question' or type = 'Discussion'") 
-  end
-
-  def followed_researches
-    followed_posts.where(type: 'Research')
-  end
-
-  # Retrun full name
-  def fullname
-    "#{first_name} #{last_name}"
-  end
-
-  def followed_questions_and_discussions
-    followed_posts.where("type = 'Question' or type = 'Discussion'") 
-  end
-
-  def followed_researches
-    followed_posts.where(type: 'Research')
-  end
-
-  # Retrun full name
-  def fullname
-    "#{first_name} #{last_name}"
-  end
 end
