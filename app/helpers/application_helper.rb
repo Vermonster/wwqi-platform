c23e6e0e17feeed4ec177176629b41b5ee74ec2c
Kyung Bae Park <kyungbae@gmail.com>
2013-05-02 13:58:58 -0400

    Add Gravatar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,4 +33,9 @@ module ApplicationHelper
       end.join('\n').html_safe
     end    
   end
+  def avatar_url(user)
+    default_url = "#{root_url}images/guest.png"
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+  end
 end
