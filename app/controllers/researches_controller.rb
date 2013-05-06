dc5bac4317726c07ace4f7900ffc2b32ffd0bb76
Gregory Salmon <salmonthefish@gmail.com>
2013-05-06 11:23:08 -0400

    fix research resource appearing as post on new

--- app/controllers/researches_controller.rb
+++ app/controllers/researches_controller.rb
@@ -1,3 +1,10 @@
 class ResearchesController < PostsController
   defaults resource_class: Research
+
+  def build_resource
+    @research = Research.new
+    @research.items.build
+    @research.collaborators.build
+    set_resource_ivar(@research)
+  end
 end
