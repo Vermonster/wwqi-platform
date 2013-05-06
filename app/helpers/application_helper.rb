ba0432b24fcf4c320c03aa3590d16f9b37258138
Kyung Bae Park <kyungbae@gmail.com>
2013-05-06 15:46:57 -0400

    Add an avatar links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,8 +34,7 @@ module ApplicationHelper
     end    
   end
   def avatar_url(user)
-    default_url = "#{root_url}images/guest.png"
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
+    "http://gravatar.com/avatar/#{gravatar_id}.png"
   end
 end
