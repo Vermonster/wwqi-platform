958db63a5d8c23a828069a8330dfd78686ce7874
Kyung Bae Park <kyungbae@gmail.com>
2013-05-19 17:00:34 -0400

    Edit comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -9,8 +9,9 @@ class Comment < ActiveRecord::Base
   accepts_nested_attributes_for :items, reject_if: :all_blank, allow_destroy: true
 
   validates :details, :commentable_id, :user_id, presence: true
+  validate :missing_items
 
-  before_save :search_and_add_items
+  before_validation :search_and_add_items
 
   def commentable_creator
     commentable.try(:creator) || commentable.user
@@ -29,7 +30,17 @@ class Comment < ActiveRecord::Base
   def collect_item_info(num)
     json_data = open "http://api.searchbox.io/api-key/7a575ca5d7081da4cf55e748d46686e4/wwqi-search-dev/item/_search?q=#{num}"
     parsed_data = ActiveSupport::JSON.decode(json_data)
-    source = parsed_data["hits"]["hits"][0]["_source"]
-    return { name: source["title_en"], thumbnail: source["thumbnail"], accession_no: source["accession_num"], url: source["url_en"] }
+    if parsed_data["hits"]["total"] < 1
+      return { name: "Not Found", thumbnail: "flag.png", accession_no: num, url: "http://www.qajarwomen.org/en/items/#{num}.html" }
+    else
+      source = parsed_data["hits"]["hits"][0]["_source"]
+      return { name: source["title_en"], thumbnail: source["thumbnail"], accession_no: source["accession_num"], url: source["url_en"] }
+    end
+  end
+
+  def missing_items
+    self.items.each do |item|
+      self.errors.add(:base, "The #{items.index(item).ordinalize} item is not found in Qajar Women site.") if item[:name] == "Not Found"
+    end
   end
 end
