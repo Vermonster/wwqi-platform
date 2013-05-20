d6a3c22b0b9fc71feff061c45d38c9180fe3fd1b
Gregory Salmon <salmonthefish@gmail.com>
2013-05-20 16:20:11 -0400

    traverse to correct element from .fileselectbutton

--- app/assets/javascripts/add_file.js
+++ app/assets/javascripts/add_file.js
@@ -1,19 +1,20 @@
 $(function(){
-  
-  $('.file-upload').on('click', '.fileselectbutton', function(e){
-    console.log('hi');
-    $('.upload').trigger('click');
+  $('.file-upload').on('click', '.fileselectbutton', function(e) {
+    $(e.currentTarget)
+      .parent()
+      .siblings('.file')
+      .find('.upload').trigger('click');
   });
     
-  $('.file-upload').on('change', '.upload', function(e){
+  $('.file-upload').on('change', '.upload', function(e) {
     var val = $(this).val();
     
     var file = val.split(/[\\/]/);
 
     var current = $(e.currentTarget);
-    console.log('whatever');
-    debugger;
-    current.parent().siblings('.dummyfile').find('.filename').val(file[file.length-1]);
+    current
+      .parent()
+      .siblings('.dummyfile')
+      .find('.filename').val(file[file.length-1]);
   });
-
 });
