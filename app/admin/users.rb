84f17213a4bac0423a717d5405f90e3aa7189e63
Kathryn Bennett <kbennett13@college.harvard.edu>
2013-05-22 14:13:42 -0400

    Fix routes for admin Users.

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -7,7 +7,8 @@ ActiveAdmin.register User do
     column :id
     column :first_name
     column :last_name
-    column :email                     
+    column :email
+    column :is_admin                     
     column :current_sign_in_at        
     column :last_sign_in_at           
     column :created_at             
@@ -16,6 +17,18 @@ ActiveAdmin.register User do
   
   filter :first_name
   filter :last_name
+  filter :is_admin
   filter :email 
   
+  form do |f|                         
+    f.inputs "Admin Details" do
+      f.input :first_name
+      f.input :last_name
+      f.input :email                  
+      f.input :password               
+      f.input :password_confirmation
+      f.input :is_admin  
+    end                               
+    f.actions                         
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(resource)
-    current_admin_user ? admin_root_path : my_profile_path
+    my_profile_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
     root_path
+  end
+  
+  def authenticate_admin
+    current_user && current_user.is_admin? || redirect_to("/")
+  end
+  
+  def current_admin_user
+    current_user && current_user.is_admin ? current_user : nil
   end
 end
