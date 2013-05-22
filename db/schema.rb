de821295ffd8bc9ada7b5a5be899a81f0b16b539
Kathryn Bennett <kbennett13@college.harvard.edu>
2013-05-22 15:53:46 -0400

    Remove AdminUser table from database.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130522171237) do
+ActiveRecord::Schema.define(:version => 20130522195025) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
