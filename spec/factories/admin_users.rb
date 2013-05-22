68f973d9308882d40eb01e13f2abd2530681f5cc
Kathryn Bennett <kbennett13@college.harvard.edu>
2013-05-22 11:21:50 -0400

    AdminUser specs.

--- spec/factories/admin_users.rb
+++ spec/factories/admin_users.rb
@@ -2,6 +2,8 @@
 
 FactoryGirl.define do
   factory :admin_user do
+    first_name { generate(:string) }
+    last_name { generate(:string) }
     email { generate(:email) }
     password "password"
     password_confirmation "password"
--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe AdminUser do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { create(:admin_user) }
+  
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:email) }
+  it { should respond_to(:password) }
+  it { should validate_uniqueness_of(:email) }
+  
 end
