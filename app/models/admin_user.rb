71d563acd72033c922a66024f2a9f16febeb485a
Kathryn Bennett <kbennett13@college.harvard.edu>
2013-05-22 11:21:36 -0400

    Add validation to AdminUser.

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,11 +1,13 @@
 class AdminUser < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  
+  validates :first_name, :last_name, :email, presence: true
+  validates :password, confirmation: true
+  validates :password, presence: true, on: :create
+  validates :email, uniqueness: true
+  
 end
