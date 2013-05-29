689dff4881c7b1ccad51bd8942f25128d43bc971
Meg Broughton <meg@vermonster.com>
2013-05-29 13:58:20 -0400

    make jquery ui dialog responsive

--- app/assets/javascripts/terms.js
+++ app/assets/javascripts/terms.js
@@ -32,23 +32,72 @@ function initializeTermsDialog() {
   var $link = $("#legal_terms").one('click', function() {
     $dialog.dialog({
         //open: function(event, ui) { $(".ui-dialog-titlebar-close").show(); },
+        autoOpen: true,
         closeOnEscape: true,
         title: "Terms",
-        width: 600,
+        maxWidth: 600,
+        width: 'auto', //overcomes width: 'auto and maxWidth bug
         height: 500,
+        fluid: true,
         draggable: false,
         resizable: false,
         modal: true,
+        open: function(event, ui){
+          fluidDialog();
+        }
       });
+
     $link.click(function() {
       $dialog.dialog('open');
-
       return false;
     });
+
     $close.click(function() {
       $dialog.dialog('close');
     });
 
     return false;
   });
+}
+
+// run function on all dialog opens
+$(document).on("dialogopen", ".ui-dialog", function (event, ui) {
+    fluidDialog();
+});
+
+// remove window resize namespace
+$(document).on("dialogclose", ".ui-dialog", function (event, ui) {
+    $(window).off("resize.responsive");
+});
+
+function fluidDialog() {
+    var $visible = $(".ui-dialog:visible");
+    // each open dialog
+    $visible.each(function () {
+        var $this = $(this);
+        var dialog = $this.find(".ui-dialog-content").data("dialog");
+        // if fluid option == true
+        if (dialog.options.maxWidth && dialog.options.width) {
+            // fix maxWidth bug
+            $this.css("max-width", dialog.options.maxWidth);
+            //reposition dialog
+            dialog.option("position", dialog.options.position);
+        }
+
+        if (dialog.options.fluid) {
+            // namespace window resize
+            $(window).on("resize.responsive", function () {
+                var wWidth = $(window).width();
+                // check window width against dialog width
+                if (wWidth < dialog.options.maxWidth + 50) {
+                    // keep dialog from filling entire screen
+                    $this.css("width", "90%");
+
+                }
+              //reposition dialog
+              dialog.option("position", dialog.options.position);
+            });
+        }
+
+    });
 }
