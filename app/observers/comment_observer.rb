e96ac61982d365651a84722652b6871b657c1f7e
Gregory Salmon <greg@vermonster.com>
2013-05-29 09:40:43 -0400

    don't create a notification if the commenter is the commentable's creator

--- app/observers/comment_observer.rb
+++ app/observers/comment_observer.rb
@@ -1,7 +1,9 @@
 class CommentObserver < ActiveRecord::Observer
   def after_create(comment)
-    comment.notifications.create(user_id: comment.commentable_creator.id)
-
+    unless comment.user == creator.commentable.creator
+      comment.notifications.create(user_id: comment.commentable_creator.id)
+    end
+  
     comment.commentable.followings.each do |f|
       comment.notifications.create(user_id: f.user.id) unless f.user == comment.user
     end
