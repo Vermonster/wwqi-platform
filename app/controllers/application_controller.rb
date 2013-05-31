520f34004bcdf10a4e2ec92521a78fd4e6510531
Gregory Salmon <greg@vermonster.com>
2013-05-31 13:14:08 -0400

    return user to the page they tried to access before auth failure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(resource)
-    my_profile_path
+    session["user_return_to"] || my_profile_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
