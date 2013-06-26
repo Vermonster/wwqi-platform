3c6e3fdc3a6dd7a3d5e1b8a6a5980fcefaaffc49
Kyung Bae Park <kyungbae@gmail.com>
2013-06-26 11:19:03 -0400

    Rebase on staging and origin master

--- spec/features/create_correction_spec.rb
+++ spec/features/create_correction_spec.rb
@@ -7,8 +7,10 @@ describe "creating corrections" do
       
       page.execute_script %Q{ $('#correction_item_relation_attributes_search').val('tin').keydown(); }
       expect { find('.ui-menu-item a#ui-id-2').text == "31g171 - Aspirin tin" }.to become_true
+
       page.execute_script %Q{ $('.ui-menu-item a:contains("31g171 - Aspirin tin")').click(); }
       expect { find('#correction_item_relation_attributes_search').value == '31g171 - Aspirin tin' }.to become_true
+      
       page.execute_script("editor.setValue('Hello world')")
       click_on 'Submit Correction'
      
@@ -30,8 +32,10 @@ describe "creating corrections" do
       
       page.execute_script %Q{ $('#correction_item_relation_attributes_search').val('tin').keydown(); }
       expect { find('.ui-menu-item a#ui-id-2').text == "31g171 - Aspirin tin" }.to become_true
+      
       page.execute_script %Q{ $('.ui-menu-item a:contains("31g171 - Aspirin tin")').click(); }
       expect { find('#correction_item_relation_attributes_search').value == '31g171 - Aspirin tin' }.to become_true
+      
       page.execute_script("editor.setValue('Hello world')")
       click_on 'Submit Correction'
       
