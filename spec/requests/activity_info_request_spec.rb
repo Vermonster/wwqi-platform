34ef69c9b754cc77b3ccfea050ca5735c8734865
Kyung Bae Park <kyungbae@gmail.com>
2013-07-22 14:14:44 -0400

    Add more integration tests

--- spec/requests/activity_info_request_spec.rb
+++ spec/requests/activity_info_request_spec.rb
@@ -12,8 +12,8 @@ describe 'json request' do
   let!(:item_related_biographies)    { create_list(:item_relation, 3, :with_biography, item: item) }
   let!(:item_related_comments)       { create_list(:item_relation, 7, :with_comment, item: item) }
   
-  before { get '/activity/activity_data?accession_no=1253A24', :format => :json
-}
+  # Make a request for the activity data in json format
+  before { get '/activity/activity_data?accession_no=1253A24', :format => :json }
 
   it 'receives a success response' do
     expect(response).to be_success
@@ -22,6 +22,8 @@ describe 'json request' do
 
   it 'receives a activity data in json' do
     activity = ActiveSupport::JSON.decode(response.body)
+
+    # Test each activity data are matched as expected
     expect(activity['Question']).to eq 4
     expect(activity['Discussion']).to eq 6
     expect(activity['Research']).to eq 2
@@ -29,5 +31,19 @@ describe 'json request' do
     expect(activity['Transcription']).to eq 1
     expect(activity['Biography']).to eq 3
     expect(activity['Comment']).to eq 7
+  end
+
+  it 'receives "Not Found" if the item is not found' do
+
+    # Make a request with a wrong accession number
+    get '/activity/activity_data?accession_no=1253', :format => :json
+
+    # Test it still receives a success response from the platform
+    expect(response).to be_success
+    expect(response.header['Content-Type']).to include 'application/json'
+
+    # Test the received json data indicates that the item is not found
+    activity = ActiveSupport::JSON.decode(response.body)
+    expect(activity['Not Found']).to eq 'Not Found'
   end
 end
