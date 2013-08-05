676a4ba48f82165b11375e3914cd02a6d4dbbc29
Kyung Bae Park <kyungbae@gmail.com>
2013-08-05 14:46:01 -0400

    Revise authenticate tests

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,152 +1,124 @@
 require 'spec_helper'
 
 describe "authentication" do
-  before do
-    Warden.test_mode!
+  let(:user) { create(:user, email: 'veronica_mars@gmail.com') }
+  before { visit root_path }
+
+  context 'when signing in' do
+    it 'lets a user sign in' do
+      within('#sign-in') do 
+        fill_in 'user_email', with: user.email
+        fill_in 'user_password', with: user.password
+        click_on 'Sign In'
+      end
+
+      expect(page).to have_content('Signed in successfully.')
+    end
+
+    it 'rejects a user when password missing' do
+      within('#sign-in') do
+        fill_in 'user_email', with: user.email
+        click_on 'Sign In'
+      end
+
+      expect(page).to have_content('Invalid email or password')
+    end
+
+    it 'rejects a user when email missing' do
+      within('#sign-in') do
+        fill_in 'user_password', with: user.password
+        click_on 'Sign In'
+      end
+
+      expect(page).to have_content('You need to sign in or sign up before continuing.')
+    end
   end
 
-  describe "signing in" do
-    before do
-      create :user, email: 'veronica_mars@gmail.com'
-    end
+  context 'when signing up' do
+    it 'lets a user signs up' do
+      fill_sign_up('Veronica', 'Mars', 'veronica_mars@gmail.com', 'apples', 'apples', true)
+      click_on 'Sign Up'
+      sleep 3
 
-    it "lets me sign in" do
-      visit '/'
-      within('#sign-in-main') do
-        fill_in 'user_email', with: 'veronica_mars@gmail.com'
-        fill_in 'user_password', with: 'password'
-        click_on 'Sign In'
-      end
-      page.should have_content('Signed in successfully.')
-    end
-
-    it "lets me sign in" do
-      visit '/'
-      within('#sign-in-main') do
-        fill_in 'user_email', with: 'veronica_mars@gmail.com'
-        click_on 'Sign In'
-      end
-      page.should have_content('Invalid email or password.')
-      within('#sign-in-main') do
-        fill_in 'user_password', with: 'password'
-        click_on 'Sign In'
-      end
-    end
-  end
-
-  describe "signing up" do
-    it "lets me sign up" do
-      visit '/'
-      within('#sign-in-main') do
-        fill_in 'user_first_name', with: 'Veronica'
-        fill_in 'user_last_name', with: 'Mars'
-        fill_in 'sign_up_user_email', with: 'veronica_mars@gmail.com'
-        fill_in 'sign_up_user_password', with: 'apples'
-        fill_in 'user_password_confirmation', with: 'apples'
-        check 'user_terms'
-        click_on 'Sign Up'
-      end
-
-      User.count.should == 1
-      User.where(first_name: 'Veronica').first.should_not be_nil
-      page.should have_content('You have signed up successfully.')
-      current_path.should == my_profile_path
+      expect(User.count).to eq 1
+      expect(User.find_by_first_name('Veronica')).not_to be_nil
+      expect(page).to have_content('You have signed up successfully.')
+      expect(current_path).to eq my_profile_path
     end
 
     it 'requires accepting the terms' do
-      visit '/'
-      within('#sign-in-main') do
-        fill_in 'user_first_name', with: 'Veronica'
-        fill_in 'user_last_name', with: 'Mars'
-        fill_in 'sign_up_user_email', with: 'veronica_mars@gmail.com'
-        fill_in 'sign_up_user_password', with: 'apples'
-        fill_in 'user_password_confirmation', with: 'apples'
-        click_on 'Sign Up'
-      end
+      fill_sign_up('Veronica', 'Mars', 'veronica_mars@gmail.com', 'apples', 'apples', false)
+      click_on 'Sign Up'
+      sleep 3
 
-      User.count.should == 0
-      within('.terms') do
-        page.should have_content('must be accepted')
-      end
-      within('#sign-in-main') do
-        fill_in 'sign_up_user_password', with: 'apples'
-        fill_in 'user_password_confirmation', with: 'apples'
-        check 'user_terms'
-        click_on 'Sign Up'
-      end
-      User.count.should == 1
-      User.where(first_name: 'Veronica').first.should_not be_nil
-      page.should have_content('You have signed up successfully.')
-      current_path.should == my_profile_path
+      expect(User.count).to eq 0
+      expect(page).to have_content('Must be accepted')
     end
   end
-
-  describe 'singing up as an invitee' do
+  
+  context 'when an invitee visits the sign up page' do
     let!(:invitation) { create :invitation }
-    let(:email) { InvitationMailer.new_invitation(invitation).deliver }
-    it 'signs up with the invitation token' do
-      # Visit to the sign up path with an invitation token
-      visit "#{new_user_registration_path}/#{invitation.token}"
+    let(:invitation_email) { InvitationMailer.new_invitation(invitation).deliver }
 
-      within('#sign-in-main') do
-        # Test token value has been successfully filled in
-        find('#user_token').value == invitation.token
+    # Visit the link in an invitation email
+    before { visit "#{new_user_registration_path}/#{invitation.token}" }
 
-        # Fill the sign up form
-        fill_in 'user_first_name', with: 'Veronica'
-        fill_in 'user_last_name', with: 'Mars'
-        fill_in 'sign_up_user_email', with: 'veronica_mars@gmail.com'
-        fill_in 'sign_up_user_password', with: 'apples'
-        fill_in 'user_password_confirmation', with: 'apples'
-        check 'user_terms'
-        click_on 'Sign Up'
-      end
+    it 'has the invitation token' do
 
-      # Load the post created by the invitation factory
+      # Test the invitation token has copied correctly
+      expect(find('#user_token').value).to eq invitation.token
+    end
+
+    it 'lets an invitee signs up' do
+      fill_sign_up('Veronica', 'Mars', 'veronica_mars@gmail.com', 'apples', 'apples', true)
+      click_on 'Sign Up'
+      click_on 'My Collaborations'
+
       test_post = Post.find(invitation.post.id)
 
-      # Test a collaborator has been added to the post
-      test_post.collaborators.length.should == 1
+      # Test the invitee has added to the correct thread as a collaborator
+      expect(Collaborator.count).to eq 1
+      expect(test_post.collaborators.length).to eq 1
+      expect(test_post.collaborators.first.user.email).to eq 'veronica_mars@gmail.com'
 
-      # Test the added collaborator is the invitee
-      test_post.collaborators.first.user.email.should == 'veronica_mars@gmail.com'
-
-      # Test there is no collaborator added except the invitee
-      Collaborator.count.should == 1
-
-      # Test the current path is the invitee profile path
-      current_path.should == my_profile_path
-      
-      click_on 'My Collaborations'
+      # Test an invitee can see the thread on his or her profile page
       expect(page).to have_content(test_post.title)
     end
-    
   end
-  
-  describe "signing in as an admin" do
-    let!(:user) do
-      create(:user, email: 'veronica_mars@gmail.com', is_admin: true)
-    end
-    
+
+  context 'an administrator signing in' do
+    let!(:user) { create(:user, is_admin: true) }
     before do
       sign_in(user)
-      visit '/admin'
+      visit admin_root_path
     end
-  
-    it "lets me visit the admin pages" do
-      page.should have_content('Dashboard')
+
+    it 'shows the administrator page' do
+      expect(page).to have_content('Dashboard')
     end
-    
-    it "redirects to profile page on logout" do
+
+    it 'redirects to an administrator user profile page when an administrator logout' do
       click_on 'Logout'
-      current_path.should == '/me'
+
+      expect(current_path).to eq my_profile_path
+    end
+
+    it 'redirects to root when a non administartor trys to visit' do
+      sign_out
+      visit admin_root_path
+
+      expect(current_path).to eq root_path
     end
   end
-  
-  describe "visiting pages without signing in" do
-    it "redirects if there is no current user" do
-      visit '/admin'
-      current_path.should == '/'
+
+  def fill_sign_up(first, last, email, password, password_confirmation, acceptance)
+    within('#new_user') do
+      fill_in 'user_first_name', with: first
+      fill_in 'user_last_name', with: last
+      fill_in 'sign_up_user_email', with: email
+      fill_in 'sign_up_user_password', with: password
+      fill_in 'user_password_confirmation', with: password_confirmation
+      check 'user_terms' if acceptance
     end
   end
 end
