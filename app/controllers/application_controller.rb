f99999a14e5ab515b5da5388b3c059e17528df7c
Kyung Bae Park <kyungbae@gmail.com>
2013-08-06 14:50:20 -0400

    Before rebasing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :set_locale
+
   def after_sign_in_path_for(resource)
     session["user_return_to"] || my_profile_path
   end
@@ -15,5 +17,9 @@ class ApplicationController < ActionController::Base
   
   def current_admin_user
     current_user && current_user.is_admin ? current_user : nil
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
