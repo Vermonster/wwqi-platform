cf45d4cc2208f040aa6c73920d06b236ae5d8df9
Kyung Bae Park <kyungbae@gmail.com>
2013-08-15 15:49:48 -0400

    Add document upload feature for a contribution

--- spec/features/create_contribution_spec.rb
+++ spec/features/create_contribution_spec.rb
@@ -25,25 +25,49 @@ describe "Contribution creation" do
       let(:user) { create(:user) }
       before { sign_in(user) }
 
-      describe "contribution creation" do 
+      describe "contribution creation", js: true do 
         it "creates a new transcription" do
           create_and_check_contribution('Transcription', test_item)
+        end
+
+        it "creates a new transcrption with a document upload" do
+          create_and_check_contribution('Transcription', test_item, true)
         end
 
         it "creates translation creation" do
           create_and_check_contribution('Translation', test_item)
         end
 
+        it "creates a new translation with a document upload" do
+          create_and_check_contribution('Transcription', test_item, true)
+        end
+
         it "creates biography creation" do
           create_and_check_contribution('Biography', test_item)
         end
 
+        it "creates a new biography with a document upload" do
+          create_and_check_contribution('Transcription', test_item, true)
+        end
+
         # Helper to test each contribution type
-        def create_and_check_contribution(type, selected_item)
+        def create_and_check_contribution(type, selected_item, test_upload=false)
           visit new_contribution_path(type: type, item: selected_item)
           expect(page).to have_content("You are submitting a #{type.downcase} for: #{selected_item.name}")
 
           fill_in "#{type.downcase}_details", with: "Contribution test"
+
+          if test_upload 
+            pending('Fila attachment issue')
+            # proecedures for the upload document test. To make this test work,
+            # the file field must have a label name. Otherwise, the test won't
+            # find the field correctly and failed. To test, please update the
+            # file field label to 'Additional Doucments'. 
+            click_on 'Add Upload' if test_upload
+            page.execute_script("$('.file.optional.upload').toggle();") if test_upload
+            attach_file('Additional Documents', "#{Rails.root}/spec/support/Montmarte.jpg") if test_upload
+          end
+
           click_on "Submit #{type.titleize}"
 
           Contribution.count.should == 1
@@ -53,6 +77,10 @@ describe "Contribution creation" do
           expect(page).to have_content(selected_item.name)
           expect(page).to have_selector("img[src$='#{selected_item.thumbnail}']")
           expect(page).to have_content("Contribution test")
+          if test_upload
+            expect(page).to have_content('Uploaded Documents') if test_upload
+            expect(page).to have_content('Montmarte.jpg') if test_upload
+          end
 
           visit contributions_path(type: type)
           expect(page).to have_content("#{user.fullname} contributed a #{type.downcase} for #{selected_item.name}")
