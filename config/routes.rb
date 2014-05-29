915a0b4fa5890230c495ff087c6ca8104500cc5f
Beth Tenorio <anaerobeth@gmail.com>
2014-05-29 16:59:10 -0400

    scope all resources to :locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,46 +1,48 @@
 WwqiPlatform::Application.routes.draw do
   #devise_for :admin_users, ActiveAdmin::Devise.config
-  get "autocomplete/users"
+  scope "/:locale", locale: /en|fa/ do
+    get "autocomplete/users"
 
-  get '/activity/activity_data'
+    get '/activity/activity_data'
 
-  devise_for :users, controllers: { registrations: 'registrations' }
-  devise_scope :user do
-    get '/users/sign_up/:token' => 'registrations#new'
+    devise_for :users, controllers: { registrations: 'registrations' }
+    devise_scope :user do
+      get '/users/sign_up/:token' => 'registrations#new'
+    end
+
+    resources :users do
+      resources :notifications
+    end
+
+    resources :posts, controller: :threads, path: :threads do
+      resources :comments
+      resources :followings
+    end
+
+    resources :researches do
+      resources :comments
+      resources :followings
+    end
+
+    resources :contributions do
+      resources :comments
+      resources :followings
+    end
+
+    resources :corrections
+
+    resources :transcriptions, :translations, :biographies, controller: :contributions
+
+    match "/admin" => "admin/dashboard#index"
+    match "/me" => "profile#show", :as => 'my_profile'
+    match "/me/notifications" => "profile#notifications", :as => 'my_notifications'
   end
-
-  resources :users do
-    resources :notifications
-  end
-
-  resources :posts, controller: :threads, path: :threads do
-    resources :comments
-    resources :followings
-  end
-
-  resources :researches do
-    resources :comments
-    resources :followings
-  end
-
-  resources :contributions do
-    resources :comments
-    resources :followings
-  end
-
-  resources :corrections
-
-  resources :transcriptions, :translations, :biographies, controller: :contributions
-
-  match "/admin" => "admin/dashboard#index"
-  match "/me" => "profile#show", :as => 'my_profile'
-  match "/me/notifications" => "profile#notifications", :as => 'my_notifications'
 
   # scope "/:locale", locale: /en|fa/ do
   #   resources :corrections, :transcriptions, :translations, :biographies, :post, :researches
   # end
 
-  get "/:locale" => "site#index"
+  # get "/:locale" => "site#index"
   root :to => 'site#index'
 
   ActiveAdmin.routes(self)
