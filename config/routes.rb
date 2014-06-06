1baf7970b41bce2aacc54bb07cb32f8c299c8741
kyungbae <kyungbae@gmail.com>
2014-06-06 15:49:33 -0400

    remove locale from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,49 +1,41 @@
 WwqiPlatform::Application.routes.draw do
   #devise_for :admin_users, ActiveAdmin::Devise.config
-  scope "(/:locale)", locale: /en|fa/ do
-    get "autocomplete/users"
+  get "autocomplete/users"
 
-    get '/activity/activity_data'
+  get '/activity/activity_data'
 
-    devise_for :users, controllers: { registrations: 'registrations' }
-    devise_scope :user do
-      get '/users/sign_up/:token' => 'registrations#new'
-    end
-
-    resources :users do
-      resources :notifications
-    end
-
-    resources :posts, controller: :threads, path: :threads do
-      resources :comments
-      resources :followings
-    end
-
-    resources :researches do
-      resources :comments
-      resources :followings
-    end
-
-    resources :contributions do
-      resources :comments
-      resources :followings
-    end
-
-    resources :corrections
-
-    resources :transcriptions, :translations, :biographies, controller: :contributions
-
-    match "/admin" => "admin/dashboard#index"
-    match "/me" => "profile#show", :as => 'my_profile'
-    match "/me/notifications" => "profile#notifications", :as => 'my_notifications'
-    root :to => 'site#index'
+  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_scope :user do
+    get '/users/sign_up/:token' => 'registrations#new'
   end
 
-  # scope "/:locale", locale: /en|fa/ do
-  #   resources :corrections, :transcriptions, :translations, :biographies, :post, :researches
-  # end
+  resources :users do
+    resources :notifications
+  end
 
-  # get "/:locale" => "site#index"
+  resources :posts, controller: :threads, path: :threads do
+    resources :comments
+    resources :followings
+  end
+
+  resources :researches do
+    resources :comments
+    resources :followings
+  end
+
+  resources :contributions do
+    resources :comments
+    resources :followings
+  end
+
+  resources :corrections
+
+  resources :transcriptions, :translations, :biographies, controller: :contributions
+
+  match "/admin" => "admin/dashboard#index"
+  match "/me" => "profile#show", :as => 'my_profile'
+  match "/me/notifications" => "profile#notifications", :as => 'my_notifications'
+  root :to => 'site#index'
 
   ActiveAdmin.routes(self)
 end
