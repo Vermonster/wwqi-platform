b18234ac8728732db823cf23c34206ff6ca39ba6
Beth Tenorio <beth@vermonster.com>
2014-06-06 14:30:45 -0400

    wip - farsi error messages

--- spec/features/fa_sign_up_and_in_spec.rb
+++ spec/features/fa_sign_up_and_in_spec.rb
@@ -32,7 +32,7 @@ feature 'Sign-in and Sign-up form shows Persian messages', js: true do
     expect(page).to have_css('.error', text: '.تکرار با اصل مطابقت ندارد')
   end
 
-  scenario 'the user registration aggrement has been ignored' do
+  scenario 'the user registration agreement has been ignored' do
     fill_sign_up('Andrews', 'Ross', 'aross@test.com', 'aross', 'aross', false)
     click_on 'Sign Up'
     sleep 3
@@ -43,11 +43,11 @@ feature 'Sign-in and Sign-up form shows Persian messages', js: true do
 
   scenario 'when a user signs in with invalid email or password' do
     fill_sign_in(user.email, 'pass')
-    click_on 'Sign In'
+    click_button 'ورود'
     sleep 3
 
     # Invalid email or password in Persian
-    expect(page).to have_css('.alert.alert-error', exact: true, text: 'ایمیل یا رمز عبور غیر معتبر')
+    expect(page).to have_css('.alert.alert-error', exact: true, text:'ﺍیﻡیﻝ یﺍ ﺮﻣﺯ ﻊﺑﻭﺭ ﻍیﺭ ﻢﻌﺘﺑﺭ.')
   end
 
   scenario 'when a user signs in successfully' do
@@ -77,7 +77,14 @@ feature 'Sign-in and Sign-up form shows Persian messages', js: true do
     # Is invalid in Persian
     expect(page).to have_css('.error', text: '.قابل قبول نیست')
   end
- 
+  describe "without authentication" do
+    it "only allows a signed in user to create a new post" do
+      visit new_post_path
+      current_path.should == root_path 
+      page.should have_content('برای ادامه باید عضو شوید یا نام کاربری ایمیل و رمز ورود خود را وارد کنید.')
+    end
+  end
+
   def fill_sign_up(first_name, last_name, email, password, password_confirmation, accept)
     within('#registration') do
       fill_in 'نام', with: first_name, match: :first
