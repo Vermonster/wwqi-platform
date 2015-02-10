dadaf136bf3f6bab3c47a623fabc0f7348ce2217
Fion Kou <fion@vermonster.com>
2015-02-10 18:18:53 -0500

    wip

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -16,10 +16,21 @@ describe "authentication" do
     end
 
     it 'rejects a user when password missing' do
+      within('#sign-in') do
+        fill_in 'user_email', with: user.email
+        click_on 'Sign In'
+      end
 
+      expect(page).to have_content('Invalid email or password.')
     end
 
     it 'rejects a user when email missing' do
+      within('#sign-in') do
+        fill_in 'user_password', with: user.password
+        click_on 'Sign In'
+      end
+
+      expect(page).to have_content('You need to sign in or sign up before continuing.')
 
     end
 
