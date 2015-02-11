02ec7a754c327fb0468c1a7cd0c15b9dac3636d4
kyungbae <kyungbae@gmail.com>
2015-02-11 13:19:33 -0500

    second study

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -33,9 +33,39 @@ describe "authentication" do
       expect(page).to have_content('You need to sign in or sign up before continuing.')
 
     end
+  end
 
     it 'allows a user to reset forgotten password' do
+      # This test has been commented out by some reason. So, we will skip this
+      # test.
+    end
 
+  context 'when signing up' do
+    it 'requires accepting the terms' do
+      fill_sign_up('Veronica', 'Mars', 'veronica_mars@gmail.com', 'apples', 'apples', false)
+      click_on 'Sign Up'
+      # this gives 3 second delay, so our server has enough time to repsonse. We
+      # don't use this delay any more however. Just plain `find` will be do same
+      # thing. With upgraded capybara, `find` usually waits until the page loaded.
+      sleep 3
+
+      expect(User.count).to eq 0 # Explain this
+      expect(page).to have_content('Must be accepted')
+    end
+
+    it 'lets a user signs up' do
+      # fill this test
+    end
+  end
+
+  def fill_sign_up(first, last, email, password, password_confirmation, acceptance)
+    within('#new_user') do
+      fill_in 'user_first_name', with: first
+      fill_in 'user_last_name', with: last
+      fill_in 'sign_up_user_email', with: email
+      fill_in 'sign_up_user_password', with: password
+      fill_in 'user_password_confirmation', with: password_confirmation
+      check 'user_terms' if acceptance
     end
   end
 end
