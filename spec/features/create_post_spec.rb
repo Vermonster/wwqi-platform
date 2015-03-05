75794c428696b11528ac2769fde5469d69c27d05
Fion Kou <fion@vermonster.com>
2015-03-05 14:46:29 -0500

    wip
    - Fill in the first two tests. wip #3 (when user creates a new question)

--- spec/features/create_post_spec.rb
+++ spec/features/create_post_spec.rb
@@ -1,20 +1,48 @@
 require 'spec_helper'
 
 describe "post creation" do
-  describe "without authentication" do
+  context "without authentication" do
     it "only allows a signed in user to create a new post" do
+      visit posts_path
+      click_link 'Start A New Thread'
+
+      expect(page).to have_content('You need to sign in or sign up before continuing')
     end
   end
 
   context "authenticated" do
 
+    let(:user1) { create(:user) }
+
     describe "Start a new thread button" do
       it "goes to the new post form" do
+        sign_in user1
+
+        visit posts_path
+        click_link 'Start A New Thread'
+
+        expect(current_path).to eq(new_post_path)
       end
     end
 
     describe "question creation" do
       it "creates a new question" do
+        sign_in user1
+
+        visit posts_path
+        click_link 'Start A New Thread'
+
+        fill_in('Your Concise Question', with: 'What is this group for?')
+        click_on('Submit')
+
+        #check path
+        expect(current_path).to eq(post_path(0))
+        #check database
+        #check confirmation msg
+        #check user 'asked a question'
+        #check title created
+        #checkout msg created
+
       end
     end
 
