571a0773aed6410ca123044233611846e2695eca
kyungbae <kyungbae@gmail.com>
2015-03-15 14:54:01 -0400

    fourth study
    For Pairing: `profile_spec.rb`
    Work Alone: `create_research_spec.rb` and `welcome_page_spec.rb`

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -1,92 +1,34 @@
 require 'spec_helper'
 
 describe "user profile" do
-  let!(:user) do
-    create(:user)
-  end
-
-  let!(:question) do
-    create(:question, creator: user)
-  end
-
-  let!(:discussion) do
-    create(:discussion, creator: user)
-  end
-
-  let!(:research) do
-    create(:research, creator: user)
-  end
-
-  let!(:biography) do
-    create(:biography, creator: user)
-  end
-
-  before(:each) { sign_in(user) }
-  after(:each) { sign_out }
-
   describe "latest" do
     it "shows most recent posts and contributions" do
-      visit my_profile_path
-      page.should have_content(question.title)
-      page.should have_content(discussion.title)
-      page.should have_content(research.title)
-      page.should have_content(biography.person_name)
     end
   end
 
   describe "threads" do
     it "shows only questions and discussions" do
-      visit my_profile_path(type: "threads")
-      page.should have_content(question.title)
-      page.should have_content(discussion.title)
-      page.should_not have_content(research.title)
-      page.should_not have_content(biography.person_name)
     end
   end
 
   describe "researches" do
     it "shows only research" do
-      visit my_profile_path(type: "researches")
-      page.should_not have_content(question.title)
-      page.should_not have_content(discussion.title)
-      page.should have_content(research.title)
-      page.should_not have_content(biography.person_name)
     end
   end
 
   describe "contributions" do
     it "shows only contributions" do
-      visit my_profile_path(type: "contributions")
-      page.should_not have_content(question.title)
-      page.should_not have_content(discussion.title)
-      page.should_not have_content(research.title)
-      page.should have_content(biography.person_name)
     end
   end
 
   describe 'collaborations', js: true do
-    let!(:collaborate1) { create(:collaborator, user: user) }
-    let!(:collaborate2) { create(:collaborator, user: user, post: discussion) }
-    let!(:collaborate3) { create(:collaborator, user: user, post: research) }
-
     it 'has a button for collaborations' do
-      visit my_profile_path
-      expect(page).to have_link('My Collaborations')
     end
 
     it 'shows all collaborations a user participates' do
-      visit my_profile_path(type: 'collaborations')
-
-      expect(page).to have_content(collaborate1.post.title)
-      expect(page).to have_content(collaborate2.post.title)
-      expect(page).to have_content(collaborate3.post.title)
     end
 
     it 'does not show the edit button' do
-      visit my_profile_path(type: 'collaborations')
-
-      page.find_link("#{collaborate1.post.title}").hover
-      expect(page).not_to have_link('Edit')
     end
 
   end
