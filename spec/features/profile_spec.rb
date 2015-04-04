e7863e5fb5d3f13c83f96d0acb6cea05e946e6d4
Fion Kou <fion@vermonster.com>
2015-04-04 12:27:09 -0400

    Finish test for `checking questions and discussions` and test for `researches`

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -15,14 +15,6 @@ describe "user profile" do
 
       visit my_profile_path
 
-      expect(page).to have_content(question.title)
-      expect(page).to have_content(discussion.title)
-
-      expect(page).to have_content("#{user1.fullname} asked a question")
-      expect(page).to have_content("#{user1.fullname} started a discussion")
-      expect(page).to have_content('no replies')
-      expect(page).to have_content('no answers')
-
       first_post = first('.item-created')
       expect(first_post).to have_content('2 days ago')
     end
@@ -30,11 +22,40 @@ describe "user profile" do
 
   describe "threads" do
     it "shows only questions and discussions" do
+      question = create(:question, creator: user1)
+      discussion = create(:discussion, creator: user1)
+      research = create(:research, creator: user1)
+
+      visit my_profile_path
+
+      click_link('My Questions and Discussions')
+
+      expect(page).to have_content(question.title)
+      expect(page).to have_content(discussion.title)
+      expect(page).to have_content("#{user1.fullname} asked a question")
+      expect(page).to have_content("#{user1.fullname} started a discussion")
+      expect(page).to have_content('no replies')
+      expect(page).to have_content('no answers')
+      expect(page).to_not have_content(research.title)
     end
   end
 
   describe "researches" do
     it "shows only research" do
+      question = create(:question, creator: user1)
+      discussion = create(:discussion, creator: user1)
+      research = create(:research, creator: user1)
+
+      visit my_profile_path
+
+      click_link('My Research-in-Progress')
+
+      expect(page).to have_content(research.title)
+      expect(page).to have_content("#{user1.fullname} shared research in progress")
+      expect(page).to have_content('no comments')
+
+      expect(page).to_not have_content(question.title)
+      expect(page).to_not have_content(discussion.title)
     end
   end
 
